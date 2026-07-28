442b3e02e47cf4270bdefd5fad093e32d5071f64
brianalbert7 <7brianalbert@gmail.com>
2026-07-27 22:13:19 -0400

    Initial commit with starter template

---
+++ src/main/java/org/brian/aisupportagent/AiSupportAgentApplication.java
@@ -0,0 +1,13 @@
+package org.brian.aisupportagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSupportAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSupportAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/brian/aisupportagent/AiSupportAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brian.aisupportagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSupportAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
